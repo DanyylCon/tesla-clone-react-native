bc1ee75f0aac6c6d1262c9f6aa05db9461fa1d96
Danyyl <danyyl.con@gmail.com>
2022-07-11 16:00:33 +0300

    add basic styles

--- App.js
+++ App.js
@@ -28,18 +28,23 @@ const styles = StyleSheet.create({
   },
 
   carContainer: {
-
+    width: '100%',
+    height: '100%'
   },
   
   titles: {
-
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center'
   },
 
   title: {
-
+    fontSize: 40,
+    fontWeight: '500'
   },
 
   subtitle: {
-    
+    fontSize: 15,
+    color: '#5c5e62'
   }
 });
