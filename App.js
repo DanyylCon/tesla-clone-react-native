54607d0e6497acbe549e9835d1717e9d952a9df3
Danyyl <danyyl.con@gmail.com>
2022-07-11 15:39:54 +0300

    create react native project and add some basic views and texts

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.carContainer}>
+      
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,249</Text>
+        </View>
+
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +26,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+
+  },
+  
+  titles: {
+
+  },
+
+  title: {
+
+  },
+
+  subtitle: {
+    
+  }
 });
