88e1a447931edab75497e14d4ed3a059c3d257ff
Danyyl <danyyl.con@gmail.com>
2022-07-11 17:08:57 +0300

    create CarItem component

---
+++ components/CarItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CarItem = (props) => {
+    return (
+        <View>
+            <Text>Just another component</Text>
+        </View>
+    )
+}
+
+export default CarItem;
